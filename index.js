8a50a7d50692e862b3c216bad1163ab316a98326
Jorge <jorge.eacostadiaz@gmail.com>
2022-09-27 22:49:49 -0300

    I feature a new part where apears the top movies

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const url = 'https://api.themoviedb.org/3/movie/12?api_key=7c4a986b16b3b892bd7111a358d63e05&language=en-US';
 const logo = 'https://api.themoviedb.org/3/movie/75780?api_key=7c4a986b16b3b892bd7111a358d63e05&language=en-US'
 const path = 'https://image.tmdb.org/t/p/w1920_and_h600_multi_faces'
+const pathCard = 'https://image.tmdb.org/t/p/w185'
 const latestRelease = 'https://api.themoviedb.org/3/trending/all/day?api_key=7c4a986b16b3b892bd7111a358d63e05'
 const pathMovie = 'https://api.themoviedb.org/3/search/movie?api_key=7c4a986b16b3b892bd7111a358d63e05&query='
 const inputBar = document.getElementById('form');
 const collection = 'https://www.themoviedb.org/collection/';
+const hightScore = 70;
+const regularScore = 50;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -28,7 +31,8 @@ inputBar.addEventListener('submit', function(e) {
 
  const renderResults = function(data) {
 
-    const resultsDiv = document.getElementById('results');
+    const resultsDiv = document.getElementById('containerResults');
+    resultsDiv.style.display = 'flex';
     data.forEach(element => {
         const itemCard = document.createElement('div');
         itemCard.classList.add('movie');
@@ -71,4 +75,47 @@ function clickHandle (event) {
         let element = document.getElementById('message');
         element.innerHTML = `<p>${data.overview}</p>`;
     });
+}
+
+const renderTopMovies = function() {
+
+    let cardsList = document.getElementsByClassName('movieCard');
+
+    fetch(latestRelease)
+    .then(response => response.json())
+    .then((data) => {
+
+        let i = 0;
+        card = Object.values(cardsList);
+        
+        card.forEach(element => {
+            let posterPath = data.results[i].poster_path;
+            let nameInfo = data.results[i].name || data.results[i].original_title  ;
+            let releaseInfo = data.results[i].first_air_date || data.results[i].release_date;
+            let scoreInfo = data.results[i].vote_average.toFixed(1) * 10;
+            let imageUrl = pathCard+posterPath;
+
+            element.children[0].style.backgroundImage = "url("+ imageUrl +")";
+            element.children[1].innerHTML =  `
+                <p class="movieName">${nameInfo}</p>
+                <p class="releaseInfo">${releaseInfo}</p>
+                <p class="scoreBox">${scoreInfo+'%'}</p>
+            `;
+
+            i++;
+            let box = element.children[1].children[2];
+            paintBoxScore(scoreInfo, box);
+        });
+    });
+}();
+
+function paintBoxScore(value, element) {
+
+    if (value > hightScore) {
+        element.style.backgroundColor = '#58FA58';
+    } else if (value > regularScore && value < hightScore ) {
+        element.style.backgroundColor = '#F3F781';
+    } else if (value < regularScore) {
+        element.style.backgroundColor = '#FA5858';
+    }
 }
