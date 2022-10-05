9cc99f812fdd35833320f868b71c6ffe5b37d730
Jorge <jorge.eacostadiaz@gmail.com>
2022-10-05 19:11:49 -0300

    I solved all the comments made in the last pull request

--- index.js
+++ index.js
@@ -7,16 +7,11 @@ const trendingSeries = ' https://api.themoviedb.org/3/trending/tv/week?api_key=7
 const latestWeekRelease = ' https://api.themoviedb.org/3/trending/all/week?api_key=7c4a986b16b3b892bd7111a358d63e05&language=en-US&page=1';
 const pathMovie = 'https://api.themoviedb.org/3/search/movie?api_key=7c4a986b16b3b892bd7111a358d63e05&query=';
 const inputBar = document.getElementById('form');
+const resultsDiv = document.getElementById('containerResults');
 const posterPath = 'https://image.tmdb.org/t/p/w94_and_h141_bestv2';
 const collection = 'https://www.themoviedb.org/collection/';
 const highScore = 70;
 const regularScore = 50;
-const resultsDiv = document.getElementById('containerResults');
-const color = {
-    green: '#58FA58',
-    blue: '#F3F781',
-    red: '#FA5858'
-};
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -24,7 +19,6 @@ function getRandomInt(max) {
 
 // It is in charged to found the movie.
 inputBar.addEventListener('submit', function(e) {
-
     e.preventDefault();
     const name = inputBar[0].value;
     const nameCleaned = name.replace(/\s/g, '+');
@@ -44,8 +38,7 @@ inputBar.addEventListener('submit', function(e) {
 
 // Renders the message to the user about the movie was not found
 const renderMessage = function() {
-    
-    resultsDiv.style.display = 'flex';
+    resultsDiv.classList.add('showContainer');
 
     resultsDiv.innerHTML = `
         <h2 class="emptyResponse">There are no movies that matched your query.</h2>
@@ -54,19 +47,17 @@ const renderMessage = function() {
 
 // Here is where the results of the searching are shown
 const renderResults = function(data, name) {
-
-    resultsDiv.style.display = 'flex';
+    resultsDiv.classList.add('showContainer');
     resultsDiv.innerHTML = '';
 
     data.forEach(element => {
-
         const urlPath = element.poster_path;
         const posterUrl = posterPath+urlPath;
         const itemCard = document.createElement('div');
         itemCard.classList.add('movie');
 
         itemCard.innerHTML = `
-            <div class="poster"></div>
+            <img class="poster" src="${posterUrl}">
             <div class="dataMovie">
                 <div class="data">
                     <p class="titleSearch">${element.title}</p>
@@ -76,7 +67,6 @@ const renderResults = function(data, name) {
             </div>
         `;
 
-        itemCard.children[0].style.backgroundImage = "url("+ posterUrl +")";
         const bar = document.getElementById('bar');
         bar.placeholder = name;
         resultsDiv.appendChild(itemCard);
@@ -86,7 +76,6 @@ const renderResults = function(data, name) {
 
 // It is responsible for render the background of the search bar.
 const updatedBackground = function() {
-
     fetch(latestWeekRelease)
     .then(response => response.json())
     .then((data) => {
@@ -111,7 +100,6 @@ const pageLogo = function() {
 
 // This function renders the cards that are going to be show in the page.
 const renderMoviesCards = function(data, divElement) {
-    
     const divContainer = document.getElementById(divElement);
     divContainer.innerHTML = ' ';
     
@@ -127,7 +115,7 @@ const renderMoviesCards = function(data, divElement) {
         itemCard.classList.add('movieCard');
 
         itemCard.innerHTML =  `
-            <img src="" class="image"></img>
+            <img src="${imageUrl}" class="image">
             <div class="description">
                 <p class="movieName">${nameInfo}</p>
                 <p class="releaseInfo">${releaseInfo}</p>
@@ -135,8 +123,6 @@ const renderMoviesCards = function(data, divElement) {
             </div>
         `;
 
-        itemCard.children[0].src = imageUrl;
-        // itemCard.children[0].style.backgroundImage = "url("+ imageUrl +")";
         divContainer.appendChild(itemCard);
     
         const box = itemCard.children[1].children[2];
@@ -147,7 +133,6 @@ const renderMoviesCards = function(data, divElement) {
 
 // This function calls the API and get the response.
 const renderTopMovies = function(latestDayRelease, divElement) {
-
     fetch(latestDayRelease)
     .then(response => response.json())
     .then((data) => {
@@ -162,51 +147,47 @@ renderTopMovies(trendingSeries, 'secondListContainer');
 
 // This function paints the average box depending on the score.
 function paintBoxScore(value, element) {
-
     const isAverage = value > regularScore && value < highScore;
     const isHighScore = value > highScore;
     const isBadScore = value <= regularScore;
 
     if (isHighScore) {
-        element.style.backgroundColor = color.green;
+        element.classList.add('goodScore');
     }
-
+    
     if (isAverage) {
-        element.style.backgroundColor = color.blue;
+        element.classList.add('regularScore');
     }
-
+    
     if (isBadScore) {
-        element.style.backgroundColor = color.red;
+        element.classList.add('badScore');
     }
 }
 
 // This function can change the status of the topMovies list
 const newSelected = function(selected) {
-
     const element = document.getElementById('selector');
     const optionsA = document.getElementById('firstOption');
     const optionsB = document.getElementById('secondOption');
-    const react = element.getBoundingClientRect();
-    console.log(react.x);
+
     if (selected === 'b') {
-
         renderTopMovies(latestWeekRelease, 'listContainer');
-        element.style.left = 75 + "px";
-        element.style.paddingLeft = 65 + "px";
-        optionsA.style.color = "#000000";
-        optionsA.style.transition = "1.5s"
-        optionsB.style.color = "#FAFAFA";
-        optionsB.style.transition = "1.5s"
-        
-    } else if (selected === 'a') {
-        
-        renderTopMovies(latestDayRelease, 'listContainer');
-        element.style.paddingLeft = 45 + "px";
-        element.style.left = "";
-        optionsB.style.color = "#000000";
-        optionsA.style.transition = "1.5s"
-        optionsB.style.transition = "1.5s"
-        optionsA.style.color = "#FAFAFA";
+        if (!element.classList.contains('selectorRightMove')) {
+            element.classList.add('selectorRightMove');
+            optionsA.classList.replace('firstOptionMove', 'secondOptionMove');
+            optionsB.classList.replace('secondOptionMove', 'firstOptionMove');
+            return;
+        }
 
+        element.classList.replace('selectorLeftMove','selectorRightMove');
+        optionsA.classList.replace('firstOptionMove', 'secondOptionMove');
+        optionsB.classList.replace('secondOptionMove', 'firstOptionMove');
+    }
+    
+    if (selected === 'a') {
+        renderTopMovies(latestDayRelease, 'listContainer');
+        element.classList.add('selectorLeftMove');
+        optionsA.classList.replace('secondOptionMove', 'firstOptionMove');
+        optionsB.classList.replace('firstOptionMove', 'secondOptionMove');
     }
 }
